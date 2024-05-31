54d1701868a66501efc43cc3e9d3edab258edfcd
Zohar Mashiah <mashiahzohar4@gmail.com>
2024-05-31 14:10:53 +0300

    feed finshed without scolling fix

--- Web/youtube-clone/src/components/Feed/UpperButtons/UpperButtons.js
+++ Web/youtube-clone/src/components/Feed/UpperButtons/UpperButtons.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import './UpperButtons.css'
 
 export default function UpperButtons() {
   return (
-    <div>UpperButtons</div>
+    <div className='buttonsWrapper'>
+      <button className='button'>
+        <img src='utilites/camera-reels.svg' />
+      </button>
+      <button className='button'>
+        <img src='utilites/bell.svg' />
+      </button>
+      <img src='utilites/video2.png' id='profileImage'/>
+    </div>
   )
 }
