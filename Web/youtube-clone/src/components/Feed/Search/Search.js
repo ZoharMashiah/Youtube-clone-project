032200f6992603546fae9a2314f3cb0814bbe889
Zohar Mashiah <mashiahzohar4@gmail.com>
2024-06-03 01:02:23 +0300

    fix the search bar

--- Web/youtube-clone/src/components/Feed/Search/Search.js
+++ Web/youtube-clone/src/components/Feed/Search/Search.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './Search.module.css';
 import {Button, Image, InputGroup, Form} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Search({setsearchText, filterVideos}) {
   return (
-    <div id={styles.wrapper}>
+    <div id={styles.wrapper} class="input-group mb-3">
       <InputGroup className="mb-3" id={styles.input}>
         <Form.Control
           placeholder="Search"
@@ -14,6 +13,7 @@ export default function Search({setsearchText, filterVideos}) {
           id={styles.form}
           onChange={e => setsearchText(e.target.value)}
         />
+        
         <Button variant="outline-secondary" id={styles.button_addon2} onClick={filterVideos}>
           <i class='bi bi-search' id={styles.icon} />
         </Button>
