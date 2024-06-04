6c77c66eb8bf3c4c9e3a434fc714bbd776895244
Zohar Mashiah <mashiahzohar4@gmail.com>
2024-06-04 10:59:45 +0300

    remove menu button

--- Web/youtube-clone/src/components/Feed/LogoMenu/LogoMenu.js
+++ Web/youtube-clone/src/components/Feed/LogoMenu/LogoMenu.js
@@ -5,9 +5,9 @@ import Image from 'react-bootstrap/Image'
 export default function LogoMenu({setcurrentVideo,setfilterdedVideos, videos}) {
   return (
     <div className={styles.imgContainer}>
-      <button className={styles.button}>
+      {/* <button className={styles.button}>
         <i class="bi bi-list" id={styles.icon} ></i>
-      </button>
+      </button> */}
       <Image src='utilites/Image.png' rounded width="100px" height="65px" className={styles.Image} onClick={() => {
         setcurrentVideo(0)
         setfilterdedVideos(videos)
