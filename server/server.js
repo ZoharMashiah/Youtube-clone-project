0ac6ea02672682d2040dabf8f7c7b887a1190faa
yuvalfadlonn <yuvalfadlon56@gmail.com>
2024-06-26 22:42:47 +0300

    Added all the User functions and a connection to the server and the mongoDB

--- server/server.js
+++ server/server.js
@@ -4,22 +4,24 @@ const cors = require("cors");
 const path = require("path");
 const fileUpload = require("express-fileupload");
 const jwt = require("jsonwebtoken");
+const userRouter = require("./routes/Users");
 const session = require("express-session");
 const mongoose = require("mongoose");
 
 // here the uri to the mongoose, its gonna throw and print an error but wont stop the server
-const mongoURI = "mongodb+srv://blah blah uri here";
+const mongoURI = "mongodb+srv://zoharmashiah:V5BU8uOWvDTWTPr6@zohar.lnkxns4.mongodb.net/api";
 
 const videoRoutes = require("./routes/videoRoutes");
 
 const app = express();
+
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../Web/youtube-clone/build")));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload());
-app.use(videoRoutes);
+//app.use(videoRoutes);
 
 mongoose
   .connect(mongoURI, {
@@ -28,6 +30,7 @@ mongoose
   })
   .then(() => {
     console.log("Connected to mongoose");
+    app.use("/api/users", userRouter);
   })
   .catch((error) => {
     console.log("Error connecting to mongoose: ", error);
