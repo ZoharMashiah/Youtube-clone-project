b27b507fac0c16f8a628e045de30d630bd2c2498
Omri Levi PC <omrilevi.11@gmail.com>
2024-06-27 21:42:01 +0300

    user template

--- server/models/User.js
+++ server/models/User.js
@@ -32,4 +32,49 @@ const userSchema = new Schema({
   },
 });
 
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   firstName: {
+//     type: String,
+//     required: true,
+//   },
+//   middleName: {
+//     type: String,
+//   },
+//   lastName: {
+//     type: String,
+//     required: true,
+//   },
+//   birthdate: {
+//     type: String,
+//     required: true,
+//   },
+//   photo: {
+//     type: String,
+//   },
+//   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "video" }],
+//   settings: {
+//     type: Object,
+//     required: true,
+//   },
+//   history: [{ type: mongoose.Schema.Types.ObjectId, ref: "video" }],
+//   settings: {
+//     type: Object,
+//     required: true,
+//   },
+//   likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+//   settings: {
+//     type: Object,
+//     required: true,
+//   },
+//   dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+//   settings: {
+//     type: Object,
+//     required: true,
+//   },
+// });
+
 module.exports = mongoose.model("User", userSchema);
