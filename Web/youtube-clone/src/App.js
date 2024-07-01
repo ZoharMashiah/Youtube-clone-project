404b3f23c3843a2aa2c4cb6d091f75fbd6a4b7b8
Zohar Mashiah <mashiahzohar4@gmail.com>
2024-07-01 14:03:22 +0300

    open the structe to the user page

--- Web/youtube-clone/src/App.js
+++ Web/youtube-clone/src/App.js
@@ -6,6 +6,7 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import VideoDisplay from "./components/WatchVid/VideoDisplay/VideoDisplay";
 import AppContext from "./AppContext";
+import UserPage from "./pages/UserPage/UserPage";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -24,6 +25,7 @@ export default function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<Feed />} />
             <Route path="/users/:userId/videos/:videoId" element={<VideoDisplay />} />
+            {/* <Route path="/" element={<UserPage />} /> */}
           </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
