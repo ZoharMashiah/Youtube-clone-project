89fe49643b9d121e850921cb9459611b90a815ef
Omri Levi PC <omrilevi.11@gmail.com>
2024-07-04 13:43:14 +0300

    added new files

---
+++ Web/youtube-clone/src/components/WatchVid/Description/Description.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Description.module.css";
+
+export default function Description({ currentVideo }) {
+  console.log("******************", currentVideo);
+  const views = currentVideo.views;
+  let viewers =
+    views > 999 ? (views > 999999 ? (views / 1000000).toFixed(0) + "M" : (views / 1000).toFixed(0) + "K") : views;
+  let time = ((Date.now() - new Date(currentVideo.publication_date).getTime()) / 60000).toFixed(0);
+  let timeStr =
+    time > 60
+      ? time > 1140
+        ? time > 43200
+          ? time > 525600
+            ? (time / 525600).toFixed(0) + " years ago"
+            : (time / 43200).toFixed(0) + " months ago"
+          : (time / 1140).toFixed(0) + " days ago"
+        : (time / 60).toFixed(0) + " hours ago"
+      : time + " minutes ago";
+
+  return (
+    <div className={styles.descriptionWrapper}>
+      <p className={styles.views}>
+        {viewers} views · {timeStr}
+      </p>
+
+      <p className={styles.description}>{currentVideo.description}</p>
+    </div>
+  );
+}
