ed8258f1f64e3bd0df5af0a7f2f521f7d648b46e
Omri Levi PC <omrilevi.11@gmail.com>
2024-07-05 19:18:14 +0300

    update views instead of refetching

--- Web/youtube-clone/src/pages/VideoDisplay/VideoDisplay.js
+++ Web/youtube-clone/src/pages/VideoDisplay/VideoDisplay.js
@@ -34,7 +34,13 @@ export default function VideoDisplay() {
       console.log("Fetching video for creator:", userId, "video:", videoId);
       const address = `/api/users/${userId}/videos/${videoId}`;
       const response = await axios.get(address);
+
       setCurrentVideo(response.data);
+      setVideoList((prevVideoList) =>
+        prevVideoList.map((video) =>
+          video._id === response.data._id ? { ...video, views: response.data.views } : video
+        )
+      );
     } catch (error) {
       console.error("Error fetching video:", error);
     } finally {
