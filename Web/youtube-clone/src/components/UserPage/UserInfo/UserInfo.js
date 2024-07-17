b866a12b475d2632d2604bb4be65fe62c53bfb63
Omri Levi PC <omrilevi.11@gmail.com>
2024-07-17 12:52:52 +0300

    added a edit user

--- Web/youtube-clone/src/components/UserPage/UserInfo/UserInfo.js
+++ Web/youtube-clone/src/components/UserPage/UserInfo/UserInfo.js
@@ -1,25 +1,42 @@
 import React from "react";
 import { useState, useEffect, useContext } from "react";
-import Button from "react-bootstrap/Button";
 import { Image } from "react-bootstrap";
 import { AppContext } from "../../../AppContext";
 import { useNavigate } from "react-router-dom";
 import authAxios from "../../../util/authAxios";
+import "./UserInfo.css";
 
 export default function UserInfo({ userId }) {
   const [show, setShow] = useState(false);
   const [userData, setUserData] = useState({});
   const { currentUser, setCurrentUser } = useContext(AppContext);
+  const [isEditing, setIsEditing] = useState(false);
   const navigate = useNavigate();
-
-  const handleClose = () => {
-    setShow(false);
-  };
-  const handleShow = () => {
-    setShow(true);
-  };
+  const [editableData, setEditableData] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
   const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+  const handleEdit = () => {
+    setIsEditing(true);
+    setEditableData({
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      birthdate: userData.birthdate,
+    });
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await authAxios.patch(`/api/users/${userId}`, editableData);
+      setUserData(response.data);
+      setIsEditing(false);
+    } catch (error) {
+      alert("Error updating user information!");
+    }
+  };
 
   const handleDelete = async () => {
     try {
@@ -44,30 +61,48 @@ export default function UserInfo({ userId }) {
   }, []);
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className="userInfo">
       <Image src={userData.photo} width="100px" height="100px" roundedCircle alt="User profile" />
       <h4>{userData.username}</h4>
-      <div>
-        <>
-          <p style={{ textDecoration: "underline" }} onClick={handleShow}>
-            Details
-          </p>
-
-          <div show={show} onHide={handleClose}>
-            <p style={{ "word-wrap": "break-word" }}>First Name: {userData.firstName}</p>
-            <p style={{ "word-wrap": "break-word" }}>Last Name: {userData.lastName}</p>
-            <p style={{ "word-wrap": "break-word" }}>
-              Birthdate: {new Date(userData.birthdate).getDay()}/{new Date(userData.birthdate).getMonth()}/
-              {new Date(userData.birthdate).getFullYear()}
-            </p>
-            {currentUser && currentUser._id === userId && (
-              <Button variant="danger" onClick={handleDelete}>
-                Delete User
-              </Button>
-            )}
+      {isEditing ? (
+        <div className="editSection">
+          <input
+            type="text"
+            className="form-control mb-3 input-field no-padding"
+            value={editableData.firstName}
+            onChange={(e) => setEditableData({ ...editableData, firstName: e.target.value })}
+          />
+          <input
+            type="text"
+            className="form-control mb-2 input-field no-padding"
+            value={editableData.lastName}
+            onChange={(e) => setEditableData({ ...editableData, lastName: e.target.value })}
+          />
+          <div className="userPageBtns">
+            <button className="submit-button userBtn" onClick={handleSave}>
+              Save
+            </button>
+            <button className="btn btn-secondary userBtn" onClick={() => setIsEditing(false)}>
+              Cancel
+            </button>
           </div>
+        </div>
+      ) : (
+        <>
+          <p>First Name: {userData.firstName}</p>
+          <p>Last Name: {userData.lastName}</p>
+          {currentUser && currentUser._id === userId && (
+            <div className="userPageBtns">
+              <button className="submit-button userBtn" onClick={handleEdit}>
+                Edit User
+              </button>
+              <button className="btn btn-danger userBtn" onClick={() => handleDelete}>
+                Delete User
+              </button>
+            </div>
+          )}
         </>
-      </div>
+      )}
     </div>
   );
 }
